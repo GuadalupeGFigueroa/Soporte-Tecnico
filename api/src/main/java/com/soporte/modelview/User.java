a8fe9edafe2f229a5e5199a7b474622d634a7ec7
mercyluz <mercyluz@hotmail.com>
2024-10-08 13:00:19 +0200

    "cambios"

--- api/src/main/java/com/soporte/modelview/User.java
+++ api/src/main/java/com/soporte/modelview/User.java
@@ -1,5 +1,50 @@
 package com.soporte.modelview;
 
-public class User {
-    
+public class user {
+    private int id_usuario;
+    private String username;
+    private String password;
+
+    // Constructor with parameters
+    public user (int id_usuario, String username, String password) {
+        this.id = id_usuario;
+        this.name = username;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario= id_usuario;
+    }
+
+    public String getUserame() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id_usuario +
+                ", name='" + username + '\'' +
+                ", password=" + password +
+                '}';
+    }
 }
+
+
---
+++ api/src/main/java/com/soporte/modelview/client.java
@@ -0,0 +1,58 @@
+package com.soporte.modelview;
+
+public class client {
+    private int id_cliente;
+    private int id_solicitud;
+    private String nombre;
+    private String apellido;
+
+    // Constructor with parameters
+    public EmployedUser (int id_cliente, int id_solicitud, String nombre, String apellido) {
+        this.id_cliente = id_cliente;
+        this.id_solicitud = id_solicitud;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    // Getters and Setters
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+    public int getId_solicitud() {
+        return id_solicitud;
+    }
+
+    public void setId_solicitud(int id_solicitud) {
+        this.id_solicitud = id_solicitud;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id_cliente +
+                "id_solicitud=" + id_solicitud +
+                ", name='" + nombre + '\'' +
+                ", password=" + apellido +
+                '}';
+    }
+}
