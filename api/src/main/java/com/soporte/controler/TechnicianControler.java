16d682d9a2bcf75d82fa7dd9424eff9412af8f7a
Juan Camilo Garcia Cuevas <juanca.cuevas.05@outlook.com>
2024-10-08 14:38:20 +0200

    technician table created

--- api/src/main/java/com/soporte/controler/TechnicianControler.java
+++ api/src/main/java/com/soporte/controler/TechnicianControler.java
@@ -1,5 +1,32 @@
 package com.soporte.controler;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class TechnicianControler {
-    
+
+    public void listarSolicictudes() {
+        String sql = "SELECT * FROM tecnico ( "
+                    + "ORDER BY fecha_solicitud DESC)";
+        
+        try (Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            ResultSet result = pstmt.executeQuery();
+            System.out.println("Lista de solicictudes por fecha: ");
+            while (result.next()) {
+                System.out.println("ID_Solicitud: " + result.getInt("ID_Solcitud") +
+                                    "Descripción: " + result.getString("Descripcion") +
+                                    "Tema: " + result.getString("tema") +
+                                    "Fecha de creacion: " + result.getDate("Fecha_Creacion") +
+                                    "Fecha de completado: " + result.getDate("Fecha_Completado") +
+                                    "Prioridad: " + result.getString("Prioridad"));
+            }
+        } catch (Exception e) {
+            System.out.println("Upss, algo salió mal :(");
+        }
+    }
 }
--- api/src/main/java/com/soporte/createTables/TechnicianTable.java
+++ api/src/main/java/com/soporte/createTables/TechnicianTable.java
@@ -1,1 +1,47 @@
 package com.soporte.createTables;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class TechnicianTable {
+    public static void createTechnicianTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS tecnico ("
+                + "ID_ecnico INT AUTO_INCREMENT PRIMARY KEY,"
+                + "ID_solicitud INT FOREIGN KEY"
+                + "name VARCHAR(255) NOT NULL,"
+                + "apellido INT NOT NULL)";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+            Statement stmt = connection.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("Table 'tecnico' creado exitosamente :)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void checkTableExists() {
+        String sql = "SELECT * FROM tecnico";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            ResultSet result = pstmt.executeQuery();
+            System.out.println("La tabla 'tecnico' existe y contiene datos:");
+            while (result.next()) {
+                System.out.println("ID_Solicitud: " + result.getInt("ID_Solcitud") +
+                                    "Descripción: " + result.getString("Descripcion") +
+                                    "Tema: " + result.getString("tema") +
+                                    "Fecha de creacion: " + result.getDate("Fecha_Creacion") +
+                                    "Fecha de completado: " + result.getDate("Fecha_Completado") +
+                                    "Prioridad: " + result.getString("Prioridad"));
+            }
+        } catch (SQLException e) {
+            System.out.println("La tabla 'persons' no existe.");
+        }
+    }
+
+}
